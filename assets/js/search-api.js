74cb2bf940780d64b3e2a6b1fedbc674ff401d08
valentina-b <valentina.bedi@gmail.com>
2020-04-12 10:50:34 +0000

    Create fetchWeatherData function

--- assets/js/search-api.js
+++ assets/js/search-api.js
@@ -1,1 +1,9 @@
-// 
+function fetchWeatherData(event) {
+    var cityInput = $("#earch-api").val();
+    if (!cityInput) {
+        $("#search-label").text("Not found - try again!");
+        return;
+    }
+
+    $("#section-content").first();
+}
