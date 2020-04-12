d05250b9cec53ef9d2c5e83e65885ab129210a9f
valentina-b <valentina.bedi@gmail.com>
2020-04-12 11:52:46 +0000

    Change js loader code into css code

--- assets/js/search-api.js
+++ assets/js/search-api.js
@@ -2,7 +2,7 @@ function fetchWeatherData(event) {
     var cityInput = $("#earch-api").val()
     if (!cityInput) {
         $("#search-label").text("Not found - try again!");
-        $("#loader").removeClass("line").html(`<img src="assets/images/svg-loader.svg" alt="Loader image">`)
+        $("#loader").removeClass("line").addClass("loader");
         return;
     }
 
