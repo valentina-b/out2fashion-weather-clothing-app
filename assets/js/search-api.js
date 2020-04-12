dc3cde0f42e226b0b40ce00b1da992280025e0ec
valentina-b <valentina.bedi@gmail.com>
2020-04-12 11:21:43 +0000

    Add loader image

--- assets/js/search-api.js
+++ assets/js/search-api.js
@@ -1,7 +1,8 @@
 function fetchWeatherData(event) {
-    var cityInput = $("#earch-api").val();
+    var cityInput = $("#earch-api").val()
     if (!cityInput) {
         $("#search-label").text("Not found - try again!");
+        $("#loader").removeClass("line").html(`<img src="assets/images/svg-loader.svg" alt="Loader image">`)
         return;
     }
 
