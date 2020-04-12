f4d80dffce0ad914ab15a410345ed3a0ec05fb87
valentina-b <valentina.bedi@gmail.com>
2020-04-12 12:49:32 +0000

    Sort code (minor)

--- assets/js/search-api.js
+++ assets/js/search-api.js
@@ -1,10 +1,14 @@
 function fetchWeatherData(event) {
-    var cityInput = $("#earch-api").val()
+    var cityInput = $("#earch-api").val();
     if (!cityInput) {
         $("#search-label").text("Not found - try again!");
         $("#loader").removeClass("line").addClass("loader");
         return;
     }
+}
 
+
+function changeContent() {
+    
     $("#section-content").first();
 }
