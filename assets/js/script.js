21ac7b4fa5ef539c6de5fca3829339ca3708fc8c
valentina-b <valentina.bedi@gmail.com>
2020-04-20 16:55:27 +0000

    Change html path, create 1st api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-// 
+let apiKey = "cUKU5seD8FYw3kzd4humiPZ5JAu8Aep3";
+let locationID = [328328, 2, 3, 4]
+let londonURL = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationID[0]}?apikey=${apiKey}&language=en-us&details=true`;
+
+console.log(londonURL);
+
+
+
+
